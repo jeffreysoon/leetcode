d751504afec1aa673808e3c784e1cbe3ce6db824
jeffreysoon <jeffreysoon@gmail.com>
2020-02-26 09:18:17 -0500

    Add files via upload
    works but time complexity is big.

--- 1192. Critical Connections in a Network.py
+++ 1192. Critical Connections in a Network.py
@@ -5,46 +5,31 @@
 
 class Solution:
     def criticalConnections(self, n: int, connections: [[int]]) -> [[int]]:
+        from collections import defaultdict
         result = []
         print(connections)
+
         for index in range(len(connections)):
-            left = connections[:index]
-            right = connections[index + 1:]
-            left_path = set()
-            right_path = set()
-            for idx in range(len(left)):
-                if len(left_path) == 0:
-                    x, y = left[idx]
-                    left_path.add(x)
-                    left_path.add(y)
-                else:
-                    if any(x in left_path for x in left[idx]):
-                        x, y = left[idx]
-                        left_path.add(x)
-                        left_path.add(y)
-            for idx in range(len(right)):
-                if len(right_path) == 0:
-                    x, y = right[idx]
-                    right_path.add(x)
-                    right_path.add(y)
-                else:
-                    if any(x in right_path for x in right[idx]):
-                        x, y = right[idx]
-                        right_path.add(x)
-                        right_path.add(y)
-            if len(left_path) == n or len(right_path) == n:
-                continue
-            else:
-                path = set()
-                for x in right_path:
-                    if x in left_path:
-                        path = left_path.union(right_path)
-                        break
-                print("{} {} {}".format(left_path, right_path, path))
-                if len(path) == n:
-                    pass
-                else:
-                    result.append(connections[index])
+            tmp = connections[:index] + connections[index + 1:]
+            con_map = defaultdict(set)
+            for x, y in tmp:
+                con_map[x].add(y)
+                con_map[y].add(x)
+
+            print(con_map)
+            found = True
+            for x, y in con_map.items():
+                for m in y:
+                    if m in con_map:
+                        con_map[x] = con_map[x].union(con_map[m])
+            print(con_map)
+
+            for x, y in con_map.items():
+                if len(y) == n:
+                    found = False
+                    break
+            if found:
+                result.append(connections[index])
         print(result)
 
 
